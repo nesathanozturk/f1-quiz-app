500fd91008f7c35b7385eb84573a9516d224d54f
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-07 19:43:31 +0300

    added function

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const hiddenBtn = document.getElementById("click");
 const main = document.querySelector("main")
-const container = document.getElementById("container");
+const container = document.getElementsByClassName("container");
 
 hiddenBtn.addEventListener("click", () => {
-    main.classList.toggle("hd");
+    main.classList.toggle("hidden");
+    container.classList.add("vsb")
 })

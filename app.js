517ecbc16ad7e5ca39fd238ec788917c65ed4aab
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-07 19:37:43 +0300

    class

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const hiddenBtn = document.getElementById("click");
 const main = document.querySelector("main")
 const container = document.getElementById("container");
+
+hiddenBtn.addEventListener("click", () => {
+    main.classList.toggle("hd");
+})

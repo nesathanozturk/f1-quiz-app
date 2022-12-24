d3cdd94894d62e52db3f12487fbb6b88f170de7e
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2022-12-24 23:38:26 +0300

    refactor: the code was shortened

--- js/app.js
+++ js/app.js
@@ -1,110 +1,111 @@
 // Quiz questions and answers:
 const data = [
-    {
-        id: "Question 1",
-        question: "Who was the 2021 world champion?",
-        a: "Charles Leclerc",
-        b: "Lewis Hamilton",
-        c: "Sebastian Vettel",
-        d: "Fernando Alonso",
-        e: "Max Verstappen",
-        correct: "e",
-    },
-    {
-        id: "Question 2",
-        question: "Who won the race of 2020 Sakhir?",
-        a: "George Russell",
-        b: "Max Verstappen",
-        c: "Sergio Perez",
-        d: "Valtteri Bottas",
-        e: "Lando Norris",
-        correct: "c",
-    },
-    {
-        id: "Question 3",
-        question: "When was Formula 1 founded?",
-        a: "1980",
-        b: "1943",
-        c: "1960",
-        d: "1930",
-        e: "1950",
-        correct: "e",
-    },
-    {
-        id: "Question 4",
-        question: "Who is the team principal of Mercedes-AMG F1?",
-        a: "Toto Wolff",
-        b: "Claire Williams",
-        c: "Rob Denis",
-        d: "Zak Brown",
-        e: "Christian Horner",
-        correct: "a",
-    },
-    {
-        id: "Question 5",
-        question: "Who has replaced Sebastian Vettel in 2021?",
-        a: "Sergio Perez",
-        b: "Lando Norris",
-        c: "Lance Stroll",
-        d: "Carlos Sainz",
-        e: "Alex Albon",
-        correct: "d",
-    },
-    {
-        id: "Question 6",
-        question: "In 2013, with which car Sebastian Vettel became Formula 1 champion?",
-        a: "RB6",
-        b: "RB12",
-        c: "RB8",
-        d: "RB9",
-        e: "RB7",
-        correct: "d",
-    },
-    {
-        id: "Question 7",
-        question: "What was the Racing Point team called in 2021?",
-        a: "McLaren",
-        b: "Aston Martin",
-        c: "Mercedes-AMG",
-        d: "Alpine",
-        e: "Williams",
-        correct: "b",
-    },
-    {
-        id: "Question 8",
-        question: "How many championships does Michael Schumacher?",
-        a: "7",
-        b: "6",
-        c: "8",
-        d: "4",
-        e: "5",
-        correct: "a",
-    },
-    {
-        id: "Question 9",
-        question: "Who has the most wins ever?",
-        a: "Michael Schumacher",
-        b: "Alain Prost",
-        c: "Lewis Hamilton",
-        d: "Mika Hakkinen",
-        e: "Ayrton Senna",
-        correct: "c",
-    },
-    {
-        id: "Question 10",
-        question: "Which one of these teams aren’t in F1?",
-        a: "Ferrari",
-        b: "Porsche",
-        c: "Alpine",
-        d: "Alfa Romeo",
-        e: "Haas",
-        correct: "b",
-    },  
+  {
+    id: "Question 1",
+    question: "Who was the 2021 world champion?",
+    a: "Charles Leclerc",
+    b: "Lewis Hamilton",
+    c: "Sebastian Vettel",
+    d: "Fernando Alonso",
+    e: "Max Verstappen",
+    correct: "e",
+  },
+  {
+    id: "Question 2",
+    question: "Who won the race of 2020 Sakhir?",
+    a: "George Russell",
+    b: "Max Verstappen",
+    c: "Sergio Perez",
+    d: "Valtteri Bottas",
+    e: "Lando Norris",
+    correct: "c",
+  },
+  {
+    id: "Question 3",
+    question: "When was Formula 1 founded?",
+    a: "1980",
+    b: "1943",
+    c: "1960",
+    d: "1930",
+    e: "1950",
+    correct: "e",
+  },
+  {
+    id: "Question 4",
+    question: "Who is the team principal of Mercedes-AMG F1?",
+    a: "Toto Wolff",
+    b: "Claire Williams",
+    c: "Rob Denis",
+    d: "Zak Brown",
+    e: "Christian Horner",
+    correct: "a",
+  },
+  {
+    id: "Question 5",
+    question: "Who will replace Sebastian Vettel in 2021?",
+    a: "Sergio Perez",
+    b: "Lando Norris",
+    c: "Lance Stroll",
+    d: "Carlos Sainz",
+    e: "Alex Albon",
+    correct: "d",
+  },
+  {
+    id: "Question 6",
+    question:
+      "In 2013, with which car Sebastian Vettel became Formula 1 champion?",
+    a: "RB6",
+    b: "RB12",
+    c: "RB8",
+    d: "RB9",
+    e: "RB7",
+    correct: "d",
+  },
+  {
+    id: "Question 7",
+    question: "What will the Racing Point team called in 2021?",
+    a: "McLaren",
+    b: "Aston Martin",
+    c: "Mercedes-AMG",
+    d: "Alpine",
+    e: "Williams",
+    correct: "b",
+  },
+  {
+    id: "Question 8",
+    question: "How many championships does Michael Schumacher?",
+    a: "7",
+    b: "6",
+    c: "8",
+    d: "4",
+    e: "5",
+    correct: "a",
+  },
+  {
+    id: "Question 9",
+    question: "Who has the most wins ever?",
+    a: "Michael Schumacher",
+    b: "Alain Prost",
+    c: "Lewis Hamilton",
+    d: "Mika Hakkinen",
+    e: "Ayrton Senna",
+    correct: "c",
+  },
+  {
+    id: "Question 10",
+    question: "Which one of these teams aren’t in F1?",
+    a: "Ferrari",
+    b: "Porsche",
+    c: "Alpine",
+    d: "Alfa Romeo",
+    e: "Haas",
+    correct: "b",
+  },
 ];
 
 // Elements:
 const clickBtn = document.getElementById("click");
-const main = document.querySelector("main")
+const main = document.querySelector("main");
 const container = document.getElementById("container");
 const header = document.querySelector(".header");
 const idEl = document.getElementById("question_id");
@@ -120,8 +121,8 @@ const nextBtn = document.getElementById("next");
 
 // To hide the "main" and make the container visible:
 clickBtn.addEventListener("click", () => {
-    main.classList.toggle("hidden");
-    container.classList.add("block");
+  main.classList.toggle("hidden");
+  container.classList.add("block");
 });
 
 // Quiz questions and score:
@@ -131,73 +132,75 @@ let score = 0;
 // Functions:
 loadQuiz();
 function loadQuiz() {
-    deSelectAnswers();
+  deSelectAnswers();
 
-    const currentQuestionData = data[currentQuestion];
-    idEl.innerText = currentQuestionData.id;
-    questionEl.innerText = currentQuestionData.question;
-    ans_a.innerText = currentQuestionData.a;
-    ans_b.innerText = currentQuestionData.b;
-    ans_c.innerText = currentQuestionData.c;
-    ans_d.innerText = currentQuestionData.d;
-    ans_e.innerText = currentQuestionData.e;
-};
+  const currentQuestionData = data[currentQuestion];
+  idEl.innerText = currentQuestionData.id;
+  questionEl.innerText = currentQuestionData.question;
+  ans_a.innerText = currentQuestionData.a;
+  ans_b.innerText = currentQuestionData.b;
+  ans_c.innerText = currentQuestionData.c;
+  ans_d.innerText = currentQuestionData.d;
+  ans_e.innerText = currentQuestionData.e;
+}
 
 function deSelectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false);
-};
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
 
 function getValue() {
-    let answer;
+  let answer;
 
-    answerEls.forEach(answerEl => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        };
-    });
-    return answer;
-};
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
 
 // Previous Question:
 backBtn.addEventListener("click", () => {
-    if (currentQuestion.valueOf() === 0) {
-        alert("You can't go back more!");
-    } else {
+  if (currentQuestion.valueOf() === 0) {
+    e.preventDefault();
+  } else {
     currentQuestion--;
     loadQuiz();
-    }
+  }
 });
 
 // Next Question:
-nextBtn.addEventListener('click', () => {
-    const answer = getValue();
 
-    if (answer) {
-        if (answer === data[currentQuestion].correct) {
-            score++;
-        }
-        currentQuestion++;
+nextBtn.addEventListener("click", () => {
+  const answer = getValue();
 
-        if (currentQuestion < data.length) {
-            loadQuiz();
-        } else if (score > 7) {
-                header.innerHTML = `
-                <h1 class="js_h1 mb-2 text-4xl text-center">Very Good!</h1>
-                <h2 class="js_h2 mb-2 text-3xl text-center">You answered ${score}/${data.length} questions correctly!</h2>
-                <button class="js_btn bg-red-700 w-36 p-2 ml-48 rounded-md text-lg text-white text-center tracking-widest" onclick="location.reload()">Try again</button>
-            `;
-        } else if (score > 3 && score <= 7) {
-                header.innerHTML = `
+  if (answer) {
+    if (answer === data[currentQuestion].correct) {
+      score++;
+    }
+    currentQuestion++;
+
+    const result = `<h2 class="js_h2 mb-2 text-3xl text-center">You answered ${score}/${data.length} questions correctly!</h2>
+    <button class="js_btn bg-red-700 w-36 p-2 ml-48 rounded-md text-lg text-white text-center tracking-widest" onclick="location.reload()">Try again</button>
+    `;
+
+    if (currentQuestion < data.length) {
+      loadQuiz();
+    } else if (score > 7) {
+      header.innerHTML = `
+                <h1 class="js_h1 mb-2 text-4xl text-center">Very Good!</h1> 
+                ${result}
+                `;
+    } else if (score > 3 && score <= 7) {
+      header.innerHTML = `
                 <h1 class="js_h1 mb-2 text-4xl text-center">Not Bad!</h1>
-                <h2 class="js_h2 mb-2 text-3xl text-center">You answered ${score}/${data.length} questions correctly!</h2>
-                <button class="js_btn bg-red-700 w-36 p-2 ml-48 rounded-md text-lg text-white text-center tracking-widest" onclick="location.reload()">Try again</button>
-            `;
-        } else if (score <= 3) {
-                header.innerHTML = `
+                ${result}
+                `;
+    } else if (score <= 3) {
+      header.innerHTML = `
                 <h1 class="js_h1 mb-2 text-4xl text-center">Go watch Cars 3, not F1!</h1>
-                <h2 class="js_h2 mb-2 text-3xl text-center">You answered ${score}/${data.length} questions correctly!</h2>
-                <button class="js_btn bg-red-700 w-36 p-2 ml-48 rounded-md text-lg text-white text-center tracking-widest" onclick="location.reload()">Try again</button>
+                ${result}
             `;
-        };
-    };
+    }
+  }
 });
